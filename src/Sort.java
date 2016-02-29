64c4cc781bf6c4d6889cdb3e7df0c58758b319e9
Meryem Esa <meresa14@gmail.com>
2016-02-28 22:51:43 -0800

    initial commit

---
+++ src/Sort.java
@@ -0,0 +1,56 @@
+
+public class Sort {
+	
+	public static void main(String[] args){
+		int[] list = {5, 7, 1, 9, 2, 8};
+		
+		insertionSort(list);
+		printArray(list);
+	}
+	
+	private static void printArray(int[] arr){
+		
+		for(int i = 0; i < arr.length; i ++){
+			System.out.print(arr[i] + "\t");
+		}
+	}
+	
+	public static void selectionSort(int[] arr){
+		
+		for(int i = 0; i < arr.length; i ++){
+			int smallest = findSmallest(arr, i);
+			int temp = arr[i];
+			arr[i] = arr[smallest];
+			arr[smallest] = temp;
+		}
+	}
+	
+	private static int findSmallest(int[] arr, int startIndex) {
+		int smallest = Integer.MAX_VALUE;
+		int smallestIndex = -1;
+
+		for (int i = startIndex; i < arr.length; i++) {
+			if (arr[i] <= smallest) {
+				smallest = arr[i];
+				smallestIndex = i;
+			}
+		}
+
+		return smallestIndex;
+	}
+
+	public static void insertionSort(int[] arr) {
+
+		for (int i = 1; i < arr.length; i++) { //loop through array
+			int last = i - 1; //get the index before you
+	
+			while (last != -1 && arr[last] > arr[last + 1]) {
+
+				int temp = arr[last];
+				arr[last] = arr[last + 1];
+				arr[last + 1] = temp;
+				last--;
+			}
+		}
+	}
+}
---
+++ src/SortTest.java
@@ -0,0 +1,59 @@
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+public class SortTest {
+	int[] testArr = {3, 5, 2, 6, 0, -100, -3};
+	int[] sortedTestArr = {-100, -3, 0, 2, 3, 5, 6};
+	
+	int[] testArr2 = {Integer.MAX_VALUE, 8, 4, Integer.MIN_VALUE};
+	int[] sortedTestArr2 = {Integer.MIN_VALUE, 4, 8, Integer.MAX_VALUE};
+	
+	int[] testArr3 = {4, 3, 4, 5, -30};
+	int[] sortedTestArr3 = {-30, 3, 4, 4, 5};
+
+	//generic sorting test - helper method
+	private void sortingTest(int[] test, int[] sortedTest, String sortType){
+		int[] arr = Arrays.copyOf(test, test.length);
+		
+		if(sortType == "selection") Sort.selectionSort(arr);
+		else if(sortType == "insertion") Sort.insertionSort(arr);
+		
+		assertArrayEquals(arr, sortedTest);
+	}
+
+	//selection sort tests
+	@Test
+	public void selectionTest1(){
+		sortingTest(testArr, sortedTestArr, "selection");
+	}
+	
+	@Test
+	public void selectionTest2(){
+		sortingTest(testArr2, sortedTestArr2, "selection");
+	}
+	
+	@Test
+	public void selectionTest3(){
+		sortingTest(testArr3, sortedTestArr3, "selection");
+	}
+	
+	
+	//insertion sort tests
+	@Test
+	public void insertionTest1(){
+		sortingTest(testArr, sortedTestArr, "insertion");
+	}
+	
+	@Test
+	public void insertionTest2(){
+		sortingTest(testArr2, sortedTestArr2, "insertion");
+	}
+	
+	@Test
+	public void insertionTest3(){
+		sortingTest(testArr3, sortedTestArr3, "insertion");
+	}
+}
